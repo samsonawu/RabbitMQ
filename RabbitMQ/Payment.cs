d64d9f3b5fb7c45cb1efaa5cd19a016aa095c5af
Samson.Awu <Samson.Awu@FCMB-DGT-L2020>
2020-12-01 16:49:34 +0100

    updates

---
+++ RabbitMQ/Payment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RabbitMQ
+{
+    [Serializable]
+    public class Payment
+    {
+        public decimal AmountToPay { get; set; }
+        public string CardNumber { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ RabbitMQ/PurchaseOrder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RabbitMQ
+{
+    [Serializable]
+    public class PurchaseOrder
+    {
+        public decimal AmountToPay { get; set; }
+        public string PoNumber { get; set; }
+        public string CompanyName { get; set; }
+        public string PaymentDayTerms { get; set; }
+    }
+}
